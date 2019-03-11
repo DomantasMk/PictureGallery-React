77eeac3ee4f6a22caf2b660ea5f3b46653a847ec
DomantasMk <45693077+DomantasMk@users.noreply.github.com>
2019-03-11 13:15:54 +0200

    new file:   src/Pics/mosaic.png 	modified:   src/components/Card.js 	modified:   src/components/SearchBox.js 	modified:   src/components/SearchCountBox.js 	modified:   src/containers/App.css 	modified:   src/index.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     const {author, author_url, id} = props.obj;
     return(
-        <a target="_blank" rel="noopener noreferrer" className='tc bg-green dib br4 pa3 ma2 grow bw2 shadow-5' href={author_url}>
+        <a target="_blank" rel="noopener noreferrer" className='tc bg-washed-red dib br4 pa3 ma2 grow bw2 shadow-5' href={author_url}>
             <img alt='photo2' src={`https://picsum.photos/200/200/?image=${id}`} />
             <div style={{color:'black', textDecoration:'underline'}}>
                 <h2>{author}</h2>
--- src/components/SearchCountBox.js
+++ src/components/SearchCountBox.js
@@ -4,7 +4,7 @@ const SearchCountBox = ({countChange}) => {
     return(
         <div className='pa3 ph0 ph3-l ph3-ns pa1-m w-50 w-100-m center fr-ns fr-l fn-m' >
             <input
-            className='pa3 w-100 w-50-l w-25-ns w-50-m fl-ns fl-l fn-m ba b--green bg-lightest-blue center-m' 
+            className='pa3 w-100 w-50-l w-25-ns w-50-m fl-ns fl-l fn-m ba b--black bg-washed-red center-m' 
             type='search' 
             placeholder='Enter picture count'
             onChange={countChange}
